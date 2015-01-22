235a73976f010de5c333c86372228dd0db9e9735
Michael Westbom <totallymike@gmail.com>
2015-01-22 08:49:13 -0500

    Teach helm to ignore . and ..

--- customizations.el
+++ customizations.el
@@ -15,6 +15,11 @@
  '(global-whitespace-mode t)
  '(helm-M-x-fuzzy-match t)
  '(helm-autoresize-mode t)
+ '(helm-boring-file-regexp-list
+   (quote
+    ("\\.$" "\\.\\.$" "\\.o$" "~$" "\\.bin$" "\\.lbin$" "\\.so$" "\\.a$" "\\.ln$" "\\.blg$" "\\.bbl$" "\\.elc$" "\\.lof$" "\\.glo$" "\\.idx$" "\\.lot$" "\\.svn$" "\\.hg$" "\\.git$" "\\.bzr$" "CVS$" "_darcs$" "_MTN$" "\\.fmt$" "\\.tfm$" "\\.class$" "\\.fas$" "\\.lib$" "\\.mem$" "\\.x86f$" "\\.sparcf$" "\\.dfsl$" "\\.pfsl$" "\\.d64fsl$" "\\.p64fsl$" "\\.lx64fsl$" "\\.lx32fsl$" "\\.dx64fsl$" "\\.dx32fsl$" "\\.fx64fsl$" "\\.fx32fsl$" "\\.sx64fsl$" "\\.sx32fsl$" "\\.wx64fsl$" "\\.wx32fsl$" "\\.fasl$" "\\.ufsl$" "\\.fsl$" "\\.dxl$" "\\.lo$" "\\.la$" "\\.gmo$" "\\.mo$" "\\.toc$" "\\.aux$" "\\.cp$" "\\.fn$" "\\.ky$" "\\.pg$" "\\.tp$" "\\.vr$" "\\.cps$" "\\.fns$" "\\.kys$" "\\.pgs$" "\\.tps$" "\\.vrs$" "\\.pyc$" "\\.pyo$")))
+ '(helm-buffers-fuzzy-matching t)
+ '(helm-ff-skip-boring-files t)
  '(indent-tabs-mode nil)
  '(inhibit-startup-screen t)
  '(js2-basic-offset 2)
