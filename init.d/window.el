d3f6803daa62a137580894a4317bd6441d186974
Michael Westbom <totallymike@gmail.com>
2015-01-28 12:08:10 -0500

    Use windmove for bouncing between windows

--- init.d/window.el
+++ init.d/window.el
@@ -16,3 +16,7 @@
 
 (global-set-key (kbd "C-x 2") 'vsplit-last-buffer)
 (global-set-key (kbd "C-x 3") 'hsplit-last-buffer)
+
+(use-package windmove
+  :init (progn
+          (windmove-default-keybindings 'meta)))
