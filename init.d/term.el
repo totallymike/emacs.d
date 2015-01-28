e9fd41a1c8367d149d57ef6b511572ae59ce7c54
Michael Westbom <totallymike@gmail.com>
2015-01-28 12:08:42 -0500

    Add multiterm

--- init.d/term.el
+++ init.d/term.el
@@ -1,3 +1,7 @@
 (add-hook 'term-mode-hook
           (lambda ()
             (setq show-trailing-whitespace nil)))
+
+(use-package multi-term
+  :ensure multi-term
+  :bind ("<f5>" . multi-term-dedicated-toggle))
