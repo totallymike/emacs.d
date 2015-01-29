fa2e0d90ebd87f92cae9ea0ce435a45605137fed
Michael Westbom <totallymike@gmail.com>
2015-01-29 15:38:54 -0500

    Move rainbow stuff over

--- init.d/prog-mode.el
+++
@@ -1,9 +0,0 @@
-(use-package rainbow-identifiers
-  :ensure rainbow-identifiers
-  :config (progn
-            (add-hook 'prog-mode-hook 'rainbow-identifiers-mode)))
-
-(use-package rainbow-delimiters
-  :ensure rainbow-delimiters
-  :config (progn
-            (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)))
