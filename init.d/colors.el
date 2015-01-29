d0c9d04596c1c23fd21e5fa14d5a7458b9abb95e
Michael Westbom <totallymike@gmail.com>
2015-01-29 12:23:10 -0500

    Move some files over

--- init.d/colors.el
+++
@@ -1,8 +0,0 @@
-(defun mcw-load-theme ()
-  (if (package-installed-p 'zenburn-theme)
-      (load-theme 'zenburn)
-    (progn
-      (package-install 'zenburn-theme)
-      (mcw-load-theme))))
-
-(mcw-load-theme)
