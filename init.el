d1211c111fed7d0d54e634e2536e8d5cc5974c6b
Michael Westbom <totallymike@gmail.com>
2015-01-29 10:10:15 -0500

    Switch over to literate config file

--- init.el
+++ init.el
@@ -1,35 +1,11 @@
-(require 'package)
-(setq package-enable-at-startup nil)
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/") t)
-
-(package-initialize)
-
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-(require 'use-package)
-
-(use-package guide-key-tip
-  :ensure t
-  :init
-  (progn
-    (setq guide-key/guide-key-sequence `("C-x"
-                                         "C-c"
-                                         "C-h")
-          guide-key/recursive-key-sequence-flag t
-          guide-key/text-scale-amount 0
-          guide-key-tip/enabled nil)
-    (guide-key-mode 1)))
-
-(setq mcw-init-dir
-      (expand-file-name "init.d" user-emacs-directory))
-
-(setq custom-file
-      (expand-file-name "customizations.el" user-emacs-directory))
-(load custom-file)
-
-(if (file-exists-p mcw-init-dir)
-    (dolist (file (directory-files mcw-init-dir t "\\.el$"))
-      (load file)))
+(add-hook 'after-init-hook
+          `(lambda ()
+             ;; Remember this directory
+             ;; Only load org-mode later if we didn't load it just now
+             ,(unless (and (getenv "ORG_HOME")
+                           (file-directory-p
+                            (expand-file-name "lisp"
+                                              (getenv "ORG_HOME"))))
+                '(require 'org))
+             (org-babel-load-file
+              (expand-file-name "mwestbom.org" user-emacs-directory))))
