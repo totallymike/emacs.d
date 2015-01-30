b8dd8d2aa1b81b90794588f177ce93eff510382f
Michael Westbom <totallymike@gmail.com>
2015-01-29 22:34:29 -0500

    Made some changes

--- init.el
+++ init.el
@@ -1,11 +1,12 @@
-(add-hook 'after-init-hook
-          `(lambda ()
-             ;; Remember this directory
-             ;; Only load org-mode later if we didn't load it just now
-             ,(unless (and (getenv "ORG_HOME")
-                           (file-directory-p
-                            (expand-file-name "lisp"
-                                              (getenv "ORG_HOME"))))
-                '(require 'org))
-             (org-babel-load-file
-              (expand-file-name "mwestbom.org" user-emacs-directory))))
+(require 'package)
+
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives
+             '("org" . "http://orgmode.org/elpa/") t)
+
+(package-initialize)
+
+(org-babel-load-file
+ (expand-file-name "mwestbom.org" user-emacs-directory))
